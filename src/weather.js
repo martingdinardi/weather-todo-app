e3e4ba02d44d9fe55a1106ffd31abd07f8c9fb28
martingdinardi <martingdinardi@gmail.com>
2022-06-01 10:34:14 -0300

    api conection done and working
    Now working with local storage

--- src/weather.js
+++ src/weather.js
@@ -1,5 +1,37 @@
 const city = document.querySelector(".city");
 const select_city = document.querySelector(".select-city");
+const checkIcon = document.querySelector(".check-icon");
+const location_weather = document.querySelector(".location-weather");
+const weather_icon = document.querySelector(".weather-icon");
+//
+
+if (localStorage.getItem("Selected City")) {
+  const selectedCitySaved = localStorage.getItem("Selected City");
+  console.log(selectedCitySaved);
+}
+
+let weather = {
+  appKey: "48b287be2a3392698910b87d61c23d37",
+  fetchWeather: () => {
+    const selectedCity = select_city.firstElementChild.value;
+    localStorage.setItem("Selected City", selectedCity);
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${selectedCity}&appid=${weather.appKey}`
+    )
+      .then((res) => res.json())
+      .then((data) => weather.displayWeather(data));
+  },
+  displayWeather: async function (data) {
+    const { name } = data;
+    const { icon } = data.weather[0];
+    console.log(weather_icon);
+    console.log(data.weather[0].main);
+    city.innerHTML = `<p>${name}</p>`;
+    weather_icon.src = `https://openweathermap.org/img/wn/${icon}@2x.png`;
+    select_city.setAttribute("hidden", "");
+    city.removeAttribute("hidden");
+  },
+};
 
 city.addEventListener("click", () => {
   city.setAttribute("hidden", "");
@@ -7,4 +39,7 @@ city.addEventListener("click", () => {
   select_city.children[0].focus();
 });
 
-console.log(select_city.children[0]);
+checkIcon.addEventListener("click", async () => {
+  await weather.fetchWeather();
+  /* await weather.displayWeather(); */
+});
